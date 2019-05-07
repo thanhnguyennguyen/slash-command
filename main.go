9051a9c5e1d89af0923d28e8bb1516a388b20985
NguyenNguyen <nguyenbk92@gmail.com>
2019-05-07 21:05:26 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/nlopes/slack"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load("env.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	http.HandleFunc("/receive", slashCommandHandler)
+
+	fmt.Println("[INFO] Server listening")
+	http.ListenAndServe(":8080", nil)
+}
+
+func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !s.ValidateToken(os.Getenv("SLACK_VERIFICATION_TOKEN")) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	switch s.Command {
+	case "/slackbot":
+		params := &slack.Msg{Text: s.Text}
+		response := fmt.Sprintf("You asked for the me for %v", params.Text)
+		w.Write([]byte(response))
+
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
