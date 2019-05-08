0cea1578420e4cc0594d76f36421e888b4769bc3
NguyenNguyen <nguyenbk92@gmail.com>
2019-05-08 22:36:09 +0700

    Update response message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/nlopes/slack"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 func main() {
 	http.HandleFunc("/receive", slashCommandHandler)
 	fmt.Println("[INFO] Server listening")
-	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +28,23 @@ func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
 	switch s.Command {
 	case "/testbot":
 		params := &slack.Msg{Text: s.Text}
-		response := fmt.Sprintf(s.UserID + ", you asked for the me for %v", params.Text)
-		w.Write([]byte(response))
+		attachments := []interface{}{}
+		attachments = append(attachments, map[string]interface{}{
+			"text": " You asked me for " + params.Text,
+		})
+		response := map[string]interface{}{
+			"response_type": "in_channel",
+			"text":          "Hello <@" + s.UserID + ">",
+			"attachments":   attachments,
+		}
+		data, err := json.Marshal(response)
+		if err != nil {
+			fmt.Errorf(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
 
 	default:
 		w.WriteHeader(http.StatusInternalServerError)
