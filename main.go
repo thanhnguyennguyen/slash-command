51f53d073989bf798eaedff8a64f584caf5e75ef
NguyenNguyen <nguyenbk92@gmail.com>
2019-05-09 13:35:15 +0700

    lucky roll

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/nlopes/slack"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/nlopes/slack"
 )
 
 func main() {
@@ -45,6 +49,49 @@ func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(data)
+		break
+
+	case "/luckyroll":
+		if strings.ToLower(s.Text) == "lunch" {
+			apiKey := os.Getenv("API_KEY")
+			// search message to get thread_ts
+			url := "https://slack.com/api/search.messages?token" + apiKey + "&query='Lunch tomorrow at the CIC building'"
+
+			req, err := http.NewRequest("GET", url, bytes.NewBuffer([]byte{}))
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
+
+			body, _ := ioutil.ReadAll(resp.Body)
+			var searchMessages slack.SearchMessages
+			json.Unmarshal(body, &searchMessages)
+			channel := searchMessages.Matches[0].Channel
+			ts := searchMessages.Matches[0].Timestamp
+			url = "https: //slack.com/api/channels.replies?token" + apiKey + "&channel=" + channel + "&thread_ts=" + ts
+			req, err = http.NewRequest("GET", url, bytes.NewBuffer([]byte{}))
+
+			client = &http.Client{}
+			resp, err = client.Do(req)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
+
+			body, _ = ioutil.ReadAll(resp.Body)
+			var ms []slack.GetConversationHistoryResponse
+			json.Unmarshal(body, &ms)
+			repliers := ms[0].ReplyUsers
+			// get replier list
+			// random
+		}
+
+		break
 
 	default:
 		w.WriteHeader(http.StatusInternalServerError)
